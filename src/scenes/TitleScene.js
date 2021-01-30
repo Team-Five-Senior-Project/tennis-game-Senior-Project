49059b0674dbcd6bcf84e4aa6e24d4abd8b39545
TobinK011 <77065865+TobinK011@users.noreply.github.com>
2021-01-30 12:40:59 -0600

    Update TitleScene.js
    Animation functions added to TitleScene.js to be swapped with finalized graphics.  Tentative "Airport Tennis" log implemented.

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -1,3 +1,4 @@
+
 class TitleScene extends Phaser.Scene {
     constructor() {
         super({
@@ -7,11 +8,63 @@ class TitleScene extends Phaser.Scene {
 
     preload() {
         // TODO: replace the main screen background image
-        this.load.image('bg', 'build/assets/images/ground.png');
+        this.load.image('bg', 'assets/images/Scene2.png');
+        this.load.image('logo', 'assets/images/logo.png');
+this.load.image('Oneplayer', 'assets/images/Oneplayer.png');
     }
 
     create() {
         this.bg = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'bg');
+
+		
+let logo = this.add.image(950, 250, 'logo')
+
+let graphics = this.add.graphics({
+x: logo.x - logo.width / 2,
+y: logo.y - logo.height / 2
+})
+.fillStyle(0xffff00, 0.75)
+.setTexture('logo', undefined, 1)
+.fillRect(0, 0, logo.width, logo.height)
+
+this.tweens.add({
+targets: graphics,
+alpha: 0,
+ease: 'Cubic.easeOut',
+duration: 500,
+repeat: -1,
+yoyo: true
+});
+
+let button = this.add.image(650, 650, 'Oneplayer')
+function create() {
+
+    game.stage.backgroundColor = '#182d3b';
+
+
+
+    button = game.add.button(game.world.centerX - 10, 400, 'Oneplayer', actionOnClick, this, 2, 1, 0);
+
+    button.onInputOver.add(over, this);
+button.on('pointerdown', () => {
+            this.scene.start('GameScene', {
+                hasPlayer2: false,
+            });
+        });
+
+}
+
+function up() {
+    console.log('button up', arguments);
+}
+
+
+
+function actionOnClick () {
+
+    background.visible =! background.visible;
+
+}
 
 
 
@@ -41,7 +94,18 @@ class TitleScene extends Phaser.Scene {
             this.scene.start('GameScene', {
                 hasPlayer2: true,
 			
-});
+
+
+
+
+
+
+
+
+
+		
+		
+		});
 	});
     }
 
