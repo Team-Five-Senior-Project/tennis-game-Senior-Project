1f111d722ffb4b1388864e3b4b0ed412824cdc85
TobinK011 <77065865+TobinK011@users.noreply.github.com>
2021-01-30 12:38:52 -0600

    TitleScene.js
    Restored Original Branch

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -7,63 +7,11 @@ class TitleScene extends Phaser.Scene {
 
     preload() {
         // TODO: replace the main screen background image
-        this.load.image('bg', 'assets/images/Scene2.png');
-        this.load.image('logo', 'assets/images/logo.png');
-this.load.image('Oneplayer', 'assets/images/Oneplayer.png');
+        this.load.image('bg', 'build/assets/images/ground.png');
     }
 
     create() {
         this.bg = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'bg');
-
-		
-let logo = this.add.image(950, 250, 'logo')
-
-let graphics = this.add.graphics({
-x: logo.x - logo.width / 2,
-y: logo.y - logo.height / 2
-})
-.fillStyle(0xffff00, 0.75)
-.setTexture('logo', undefined, 1)
-.fillRect(0, 0, logo.width, logo.height)
-
-this.tweens.add({
-targets: graphics,
-alpha: 0,
-ease: 'Cubic.easeOut',
-duration: 500,
-repeat: -1,
-yoyo: true
-});
-
-let button = this.add.image(650, 650, 'Oneplayer')
-function create() {
-
-    game.stage.backgroundColor = '#182d3b';
-
-
-
-    button = game.add.button(game.world.centerX - 10, 400, 'Oneplayer', actionOnClick, this, 2, 1, 0);
-
-    button.onInputOver.add(over, this);
-button.on('pointerdown', () => {
-            this.scene.start('GameScene', {
-                hasPlayer2: false,
-            });
-        });
-
-}
-
-function up() {
-    console.log('button up', arguments);
-}
-
-
-
-function actionOnClick () {
-
-    background.visible =! background.visible;
-
-}
 
 
 
@@ -93,18 +41,7 @@ function actionOnClick () {
             this.scene.start('GameScene', {
                 hasPlayer2: true,
 			
-
-
-
-
-
-
-
-
-
-		
-		
-		});
+});
 	});
     }
 
