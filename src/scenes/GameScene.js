e0db6fdc8c7cf21d5c4d99fcda236d4d521c4746
Paul Eiche <peiche@gmail.com>
2021-02-19 10:03:00 -0600

    Fixed typos

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -163,7 +163,7 @@ class GameScene extends Phaser.Scene {
         // Collider function player 1
         this.hitThePlayer1 = (ball) => {
             this.moveVelocityX = this.moveVelocityX + 10;
-            this.moveVelocityX = this.moveVelocityX * (-1); // Change direction after contatct 
+            this.moveVelocityX = this.moveVelocityX * (-1); // Change direction after contact 
             ball.setVelocityX(this.moveVelocityX);
             
             this.moveVelocityY = Phaser.Math.Between(-1000, 1000); // Give a random Y direction when it hits the player
@@ -178,7 +178,7 @@ class GameScene extends Phaser.Scene {
         // Collider function player 2
         this.hitThePlayer2 = (ball) => {
             this.moveVelocityX = this.moveVelocityX + 10;
-            this.moveVelocityX = this.moveVelocityX * (-1); // Change direction after contatct 
+            this.moveVelocityX = this.moveVelocityX * (-1); // Change direction after contact 
             ball.setVelocityX(this.moveVelocityX);
 
             this.moveVelocityY = Phaser.Math.Between(-1000, 1000); // Give a random Y direction when it hits the player
