d05bc61dc2643ec711978b5c88527753925a0b3f
Paul Eiche <peiche@gmail.com>
2021-02-22 16:53:31 -0600

    Text color changes for game over screen

--- src/scenes/EndScene.js
+++ src/scenes/EndScene.js
@@ -24,6 +24,7 @@ class EndScene extends Phaser.Scene {
         this.gameOverText = this.add.text(this.cameras.main.centerX, 150, 'GAME OVER', {
             fontFamily: 'Roboto',
             fontSize: '100px',
+            color: '#53862D',
         }).setOrigin(0.5);
         
         let scoreP1Txt = `Your Score: ${this.score1}`;
@@ -34,10 +35,10 @@ class EndScene extends Phaser.Scene {
         } else {
             switch (this.numPlayers) {
                 case 1:
-                    winnerTextStr = this.score1 > this.score2 ? `Congratulations! Player ${this.numPlayers}` : 'Nice try!';
+                    winnerTextStr = this.score1 > this.score2 ? `Congratulations Player ${this.numPlayers}!` : 'Nice try!';
                     break;
                 case 2:
-                    winnerTextStr = `Congratulations Player ${this.score1 > this.score2 ? 1 : 2}`;
+                    winnerTextStr = `Congratulations Player ${this.score1 > this.score2 ? 1 : 2}!`;
                     break;
                 default:
                     break;
@@ -47,11 +48,13 @@ class EndScene extends Phaser.Scene {
         this.yourScoreTxtP1 = this.add.text(this.cameras.main.centerX, (this.cameras.main.centerY / 2) + 300, scoreP1Txt, {
             fontFamily: 'Roboto',
             fontSize: '75px',
+            color: '#53862D',
         }).setOrigin(0.5).setVisible(false);
 
         this.yourScoreTxtP2 = this.add.text(this.cameras.main.centerX, (this.cameras.main.centerY / 2) + 300, scoreP2Txt, {
             fontFamily: 'Roboto',
             fontSize: '75px',
+            color: '#53862D',
         }).setOrigin(0.5).setVisible(false);
 
         // Conditionally display player's score
@@ -64,11 +67,13 @@ class EndScene extends Phaser.Scene {
         this.winnerText = this.add.text(this.cameras.main.centerX, (this.cameras.main.centerY / 2) + 200, winnerTextStr, {
             fontFamily: 'Roboto',
             fontSize: '75px',
+            color: '#53862D',
         }).setOrigin(0.5);
 
-        this.restartText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 200, 'Main Menu', {
+        this.mainMenuText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 200, 'Main Menu', {
             fontFamily: 'Roboto',
             fontSize: '50px',
+            color: '#124E78',
         }).setOrigin(0.5).setInteractive({
             useHandCursor: true,
         }).on('pointerdown', () => {
@@ -80,6 +85,7 @@ class EndScene extends Phaser.Scene {
         this.resetGameText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 250, 'Reset Game', {
             fontFamily: 'Roboto',
             fontSize: '50px',
+            color: '#124E78',
         }).setOrigin(0.5).setInteractive({
             useHandCursor: true,
         }).on('pointerdown', () => {
