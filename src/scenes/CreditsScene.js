f57108fd70f8128d95fe969ba03606c993d22ee4
Paul Eiche <peiche@gmail.com>
2021-02-25 11:09:29 -0600

    Updated credits to include roles

--- src/scenes/CreditsScene.js
+++ src/scenes/CreditsScene.js
@@ -15,22 +15,34 @@ class CreditsScene extends Phaser.Scene {
     create() {
         this.bg = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'bg');
 
-        this.creditsText = this.add.text(600, 200, 'Credits', {
+        this.teamLeadTitleText = this.add.text(200, 200, 'Team Lead', {
             fontFamily: 'Roboto',
             fontSize: '75px',
             color: '#53862D',
         });
+        this.teamLeadText = this.add.text(200, 300, 'Syed Husain', {
+            fontFamily: 'Roboto',
+            fontSize: '75px',
+            color: '#124E78',
+        });
 
-        // weird indentation is necessary for newlines
-        const contributors = `Syed Husain
-Alejandro Biancucci
-Anthony Talharim
-Paul Eiche
-Michael Fiotakis
-Tobin Keruppayil
-Justin Moss
-        `;
-        this.contributorsText = this.add.text(900, 200, contributors, {
+        this.devTeamTitleText = this.add.text(200, 500, 'Development Team', {
+            fontFamily: 'Roboto',
+            fontSize: '75px',
+            color: '#53862D',
+        });
+        this.devTeamText = this.add.text(200, 600, 'Alejandro Bianucci\nPaul Eiche\nJustin Moss', {
+            fontFamily: 'Roboto',
+            fontSize: '75px',
+            color: '#124E78',
+        });
+
+        this.gameplayTitleText = this.add.text(1000, 300, 'Graphics & Gameplay', {
+            fontFamily: 'Roboto',
+            fontSize: '75px',
+            color: '#53862D',
+        });
+        this.gameplayText = this.add.text(1000, 400, 'Tobin Keruppayil\nMichael Fiotakis\nAnthony Talharim', {
             fontFamily: 'Roboto',
             fontSize: '75px',
             color: '#124E78',
